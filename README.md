93cc2c082d5a77c83f2250b8155e856e0762ff96
Filip Chalupa <chalupa.filip@gmail.com>
2021-09-22 17:59:30 +0200

    Add memory

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Webcam warp
 
+- Add `?reset` to url to forget all options.
+
 Work in progress
--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 ;(async () => {
+	const currentUrl = new URL(document.location)
+	if (currentUrl.searchParams.get('reset') !== null) {
+		localStorage.removeItem('lastDeviceId')
+		currentUrl.searchParams.delete('reset')
+		location.href = currentUrl.toString()
+	}
+
 	const $video = document.querySelector('.video')
 	const $picker = document.querySelector('.picker')
 	const $pickerCameraSelect = $picker.querySelector('select')
 
-	await navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-	const cameras = (await navigator.mediaDevices.enumerateDevices()).filter(
-		(device) => device.kind === 'videoinput',
-	)
-
-	$pickerCameraSelect.innerHTML = ''
-	$pickerCameraSelect.innerHTML += `<option>Choose</option>`
-	cameras.forEach((camera, i) => {
-		$pickerCameraSelect.innerHTML += `<option value="${camera.deviceId}">${
-			camera.label || `Camera ${i + 1}`
-		}</option>`
-	})
-
-	$pickerCameraSelect.addEventListener('change', async () => {
-		const deviceId = $pickerCameraSelect.value
+	const selectCamera = async (deviceId) => {
+		localStorage.setItem('lastDeviceId', deviceId)
 		const constraints = {
 			video: {
 				deviceId: {
@@ -29,5 +23,30 @@
 		const stream = await navigator.mediaDevices.getUserMedia(constraints)
 		$video.srcObject = stream
 		$picker.remove()
+	}
+
+	await navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+	const cameras = (await navigator.mediaDevices.enumerateDevices()).filter(
+		(device) => device.kind === 'videoinput',
+	)
+
+	const lastDeviceId = localStorage.getItem('lastDeviceId')
+	if (
+		lastDeviceId &&
+		cameras.some((camera) => camera.deviceId === lastDeviceId)
+	) {
+		selectCamera(lastDeviceId)
+	} else {
+		$pickerCameraSelect.innerHTML = ''
+		$pickerCameraSelect.innerHTML += `<option>Choose</option>`
+		cameras.forEach((camera, i) => {
+			$pickerCameraSelect.innerHTML += `<option value="${camera.deviceId}">${
+				camera.label || `Camera ${i + 1}`
+			}</option>`
+		})
+	}
+
+	$pickerCameraSelect.addEventListener('change', () => {
+		selectCamera($pickerCameraSelect.value)
 	})
 })()
