231332fea7b8a12e8a6f6b356841c289c8952f65
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-19 17:41:18 +0100

    Add link to legacy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Container, Typography } from '@mui/material'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
 	return (
@@ -13,7 +14,7 @@ export default function Home() {
 			<Container>
 				<Typography variant="h1">Webcam warp</Typography>
 				<Typography variant="h2">Pick a camera</Typography>
-				@TODO
+				<Link href="/legacy/index.html">Open legacy version</Link>
 			</Container>
 		</>
 	)
