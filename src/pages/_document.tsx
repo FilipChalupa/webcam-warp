9558799d761e5360484a730a1ac58d4e7d11f935
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-19 17:28:49 +0100

    Add base

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 				/>
+				<meta name="color-scheme" content="only dark" />
 			</Head>
 			<body>
 				<Main />
